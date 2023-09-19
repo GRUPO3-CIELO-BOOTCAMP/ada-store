199a6f061bd0ef26713744e6b63554954ccee579
Igor <igor@cincatarina.sc.gov.br>
2023-09-19 09:43:32 -0300

    feat: insere tema escuro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+
 export function App() {
-  return <h1>Hello World</h1> 
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <header className="h-12 border border-b-zinc-800 flex justify-between items-center px-8">
+        <span>ADA STORE</span>
+        <ModeToggle />
+      </header>
+    </ThemeProvider>
+  )
 }
