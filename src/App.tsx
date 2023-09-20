1ad5f278694b4b00a681b3068b46b233b1117b43
Igor <igor.tub@hotmail.com>
2023-09-20 19:42:29 -0300

    refactor: coloca navbar dentro da home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { ThemeProvider } from './components/theme-provider'
 import Home from './pages/Home'
-import { Navbar } from './components/navbar'
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Navbar
-        onInputSearch={(term) => {
-          console.log('termo da pesquisa:', term)
-        }}
-        amountProducts={4}
-      />
       <main className="flex box-border">
         <Home />
       </main>
