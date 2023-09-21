cee433801ca115674e936ae4c720ea0c33e01b90
devrosilva <rodrigosilva.info@gmail.com>
2023-09-20 21:27:36 -0300

    refactor: home component e adiciona lógica inicial para filtro por avaliação

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,14 +1,18 @@
-import { CategoriesData } from '@/types/DataTypes'
+import { CategoriesData, RatingData } from '@/types/DataTypes'
 import { Categories } from './categories'
 import { Prices } from './prices'
 import { ScoreFilter } from './score-filter'
 
-export const SideBar = ({ categories, handleProducts }: CategoriesData) => {
+export const SideBar = ({
+  categories,
+  filterByCategory,
+  filterByRating,
+}: CategoriesData & RatingData) => {
   return (
-    <div className="flex flex-col w-[248px] h-[653px] mt-[62px] ml-[32px] p-[16px] gap-[20px] box-border border-2 rounded">
-      <Categories categories={categories} handleProducts={handleProducts} />
+    <div className="flex flex-col w-min h-fit m-4 p-[16px] gap-[20px] box-border border-2 rounded">
+      <Categories categories={categories} filterByCategory={filterByCategory} />
       <Prices />
-      <ScoreFilter />
+      <ScoreFilter filterByRating={filterByRating} />
     </div>
   )
 }
